a10101d1237b9f33a79c65a71e31e29172609bc0
Quentin Sebille <quentin.sebille@gmail.com>
2025-12-15 22:26:56 +0100

    Fix recently played games

--- src/main/java/fr/trophyquest/web/service/repository/GameRepository.java
+++ src/main/java/fr/trophyquest/web/service/repository/GameRepository.java
@@ -19,17 +19,23 @@ public interface GameRepository extends JpaRepository<Game, UUID> {
     long getTotalPlayerGamesPlayed(@Param("playerId") UUID playerId);
 
     @Query(value = """
-            SELECT g.*
-            FROM app.game g
-                     INNER JOIN (SELECT pg.game_id,
-                                        pg.player_id,
-                                        pg.last_played_at AS last_played_at,
-                                        COUNT(*) AS players_count
-                                 FROM app.played_game pg
-                                 WHERE pg.last_played_at >= now() - interval '7 days'
-                                 GROUP BY pg.game_id, pg.player_id) player_count ON g.id = player_count.game_id
+            SELECT g.*,
+                   MAX(pg.last_played_at) AS last_played_at,
+                   COUNT(*) AS players_count
+            FROM app.played_game pg
+                     JOIN app.game g ON pg.game_id = g.id
+            WHERE pg.last_played_at >= now() - interval '7 days'
+            GROUP BY g.id, g.title, g.image_url, g.created_at, g.updated_at, g.aws_image_url
             ORDER BY players_count DESC, last_played_at DESC
             LIMIT :limit OFFSET :offset
             """, nativeQuery = true)
     List<Game> fetchRecentlyPlayedGames(@Param("limit") int limit, @Param("offset") int offset);
+
+    @Query(value = """
+            SELECT COUNT( DISTINCT game_id )
+            FROM app.played_game pg
+            WHERE pg.last_played_at >= now() - interval '7 days'
+            """, nativeQuery = true)
+    long countRecentlyPlayedGames();
+
 }
---
+++ src/test/java/fr/trophyquest/web/service/controllers/GameControllerIT.java
@@ -0,0 +1,37 @@
+package fr.trophyquest.web.service.controllers;
+
+import fr.trophyquest.web.service.IntegrationTestBase;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Sql(
+        scripts = "/sql/clean-db.sql",
+        executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
+)
+class GameControllerIT extends IntegrationTestBase {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @Sql(scripts = "/sql/games/game-controller-it-recently-played-endpoint-data.sql")
+    void should_return_recently_most_played_games() throws Exception {
+        mockMvc.perform(get("/api/game/recently-played?pageNumber=0&pageSize=50"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("total").value(3))
+                .andExpect(jsonPath("content").exists())
+                .andExpect(jsonPath("content", hasSize(3)))
+                .andExpect(jsonPath("content[0].title").value("Horizon"))
+                .andExpect(jsonPath("content[1].title").value("Outer Wilds"))
+                .andExpect(jsonPath("content[2].title").value("Tomb Raider"))
+        ;
+    }
+}
+
